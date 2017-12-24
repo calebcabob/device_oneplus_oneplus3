a917335d1ee945c59a0f2baacd4364e1a396e5be
rc420head <dennislebhardt79@gmail.com>
2017-12-23 23:11:10 -0600

    AquariOS

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,9 +28,9 @@ INITIAL_COPYRIGHT_YEAR=2016
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-GZOSP_ROOT="$MY_DIR"/../../..
+AQUQARIOS_ROOT="$MY_DIR"/../../..
 
-HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+HELPER="$AQUQARIOS_ROOT"/vendor/aquarios/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -38,7 +38,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AQUQARIOS_ROOT"
 
 # Copyright headers and guards
 write_headers
